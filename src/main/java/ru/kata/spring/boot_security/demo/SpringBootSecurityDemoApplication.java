5a148e8a07a7b27325a99e0383ac41de71a9e3eb
PrulleX <safronovvanya2020@gmail.com>
2024-07-08 12:29:22 +0300

    fifth commit

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -1,36 +1,13 @@
 package ru.kata.spring.boot_security.demo;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.kata.spring.boot_security.demo.configs.TestCfg;
-import ru.kata.spring.boot_security.demo.model.Users;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
+@SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestCfg.class);
-
-		Communication communication = context.getBean("communication", Communication.class);
-
-		// Добавление нового пользователя
-		Users newUser = new Users(3L, "James", "Brown", (byte) 23);
-		String saveResponse = communication.saveUser(newUser);
-		System.out.println("Save response: " + saveResponse);
-
-		// Обновление пользователя
-		Users updatedUser = new Users(3L, "Thomas", "Shelby", (byte) 23);
-		String updateResponse = communication.updateUser(updatedUser);
-		System.out.println("Update response: " + updateResponse);
-
-		// Удаление пользователя
-		String deleteResponse = communication.deleteUser(updatedUser);
-		System.out.println("Delete response: " + deleteResponse);
-
-		// Получение всех пользователей
-		List<Users> allUsers = communication.getAllUsers();
-		System.out.println(allUsers);
-
-		context.close();
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
 	}
+
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -1,66 +1,67 @@
-//package ru.kata.spring.boot_security.demo.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.web.bind.annotation.*;
-//import ru.kata.spring.boot_security.demo.model.Role;
-//import ru.kata.spring.boot_security.demo.model.User;
-//import ru.kata.spring.boot_security.demo.service.RoleService;
-//import ru.kata.spring.boot_security.demo.service.UserService;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/admin")
-//public class AdminRestController {
-//
-//    private final UserService userService;
-//    private final RoleService roleService;
-//
-//    @Autowired
-//    public AdminRestController(UserService userService, RoleService roleService) {
-//        this.userService = userService;
-//        this.roleService = roleService;
-//    }
-//
-//    @GetMapping("/current")
-//    public ResponseEntity<User> getUser(@AuthenticationPrincipal User user) {
-//        return new ResponseEntity<User>(user, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/users")
-//    public ResponseEntity<List<User>> getAllUsers() {
-//        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/roles")
-//    public ResponseEntity<List<Role>> allRoles() {
-//        return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
-//
-//        return new ResponseEntity<>(userService.showUserId(id), HttpStatus.OK);
-//    }
-//
-//    @PostMapping("/new")
-//    public ResponseEntity<HttpStatus> save(@RequestBody User user) {
-//        userService.saveUser(user);
-//        return ResponseEntity.ok(HttpStatus.OK);
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public ResponseEntity<HttpStatus> update(@RequestBody User user) {
-//        userService.updateUser(user);
-//        return ResponseEntity.ok(HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
-//        userService.deleteUser(id);
-//        return ResponseEntity.ok(HttpStatus.OK);
-//    }
-//}
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminRestController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminRestController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<User> getUser(@AuthenticationPrincipal User user) {
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<List<Role>> allRoles() {
+        return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
+
+        return new ResponseEntity<>(userService.showUserId(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<HttpStatus> save(@RequestBody User user) {
+        userService.saveUser(user);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<HttpStatus> update(@RequestBody @Valid User user, @PathVariable ("id") long id) {
+        userService.updateUser(user, id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
